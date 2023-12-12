f47abe8bed598f500a7367bfbf80fde33b1aff00
aheath <aaron.heath5447@gmail.com>
2023-12-11 22:01:47 -0500

    Add process.env.PORT to server.js. Remove comments

--- routes/apiResource.js
+++ routes/apiResource.js
@@ -12,7 +12,6 @@ const apiResource = express.Router();
 apiResource.use(express.json());
 
 apiResource.get('/notes', (req,res)=> {
-    // `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
     const notes = require('../db/db.json');
 
     // Send JSON response
@@ -20,7 +19,6 @@ apiResource.get('/notes', (req,res)=> {
 });
 
 apiResource.post('/notes', (req,res)=> {
-    // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you). -- done
 
     // read input
     let newNote = req.body; 
@@ -40,8 +38,6 @@ apiResource.post('/notes', (req,res)=> {
 })
 
 apiResource.delete('/notes/:id', (req,res)=> {
-    // `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
     const notes = require('../db/db.json');
     const id = req.params.id;
     console.log(id);
