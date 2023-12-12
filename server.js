17d192eb054446ab3b8c6dbab663ebfb89c2c3a6
aheath <aaron.heath5447@gmail.com>
2023-12-11 19:10:24 -0500

    Refactor server.js and notesResource -> apiResource.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Import dependencies
 const express = require('express');
 const fs = require('fs');
-const notesResouce = require('./routes/notesResource');
+const notesResouce = require('./routes/apiResource');
 
 // Create app
 const PORT = 3001;
@@ -12,7 +12,17 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Import routes
-app.use('/notes', notesResouce);
+app.use('/api', notesResouce);
+
+
+app.get('/', (req,res)=> {
+    // `GET *` should return the `index.html` file.
+})
+
+app.get('/notes', (req,res)=> {
+    // `GET /notes` should return the `notes.html` file.
+
+})
 
 
 // Begin app
